fd645e2f0c2463bbbe2852602094e2df258485e8
Rawan <rawanalshmsam@gmail.com>
2026-02-19 03:49:23 +0300

    complete Lap3

---
+++ apps/bookmodule/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+ name = request.GET.get("name") or "world!"
+ return render(request, "bookmodule/index.html")
+
+def index2(request, val1 = 0): #add the view function (index2)
+ return HttpResponse("value1 = "+str(val1))
+
+def viewbook(request, bookId):
+    book1 = {'id': 55, 'title': 'Continuous Delivery', 'author': 'Rawan'}
+    book2 = {'id': 666, 'title': 'Secrets of Reverse Engineering', 'author': 'HI'}
+
+    targetBook = None
+    if book1['id'] == bookId:
+        targetBook = book1
+    if book2['id'] == bookId:
+        targetBook = book2
+
+    context = {'book': targetBook}
+    return render(request, 'bookmodule/show.html', context)
--- libraryproject/settings.py
+++ libraryproject/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = os.path.join(BASE_DIR, "apps" + os.sep + "templates")
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.bookmodule',
+    'apps.usermodule',
+ 
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +60,7 @@ ROOT_URLCONF = 'libraryproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
